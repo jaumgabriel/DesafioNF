9f207eb7e9ea10fb938f6449498dac39746ed998
João Gabriel Deoclécio <jaumgabriel0102@gmail.com>
2021-08-09 22:24:31 -0300

    adição de classes

---
+++ DesafioNF_v1/Livro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioNF_v1
+{
+    class Livro
+    {
+        public string nome;
+    }
+}
